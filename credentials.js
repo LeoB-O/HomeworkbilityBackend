c8b85a01966a9af87ab2e6ea5815161abfaca31e
leob_o <liubobotb@126.com>
2019-06-23 10:40:53 +0800

    pm2 deploy

--- credentials.js
+++ credentials.js
@@ -1,29 +1,65 @@
-module.exports = {
-  mongodb: {
-    url: 'mongodb://localhost/homework',
-    options: {
-      useNewUrlParser: true,
-      useFindAndModify: false
+const credentials = {
+  production: {
+    mongodb: {
+      url: 'mongodb://localhost/homework',
+      options: {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    },
+    jwt: {
+      secret: '123456',
+      options: {
+        cookie: 'hw',
+        cookies: true,
+        cookieOptions: {domain: '', path: ''}
+      }
+    },
+    upload: {
+      baseDir: '/data/homeworks',
+      options: {
+        createParentPath: true
+      }
+    },
+    cors: {
+      options: {
+        origin: 'http://47.102.144.176',
+        credentials: true
+      }
     }
   },
-  jwt: {
-    secret: '123456',
-    options: {
-      cookie: 'hw',
-      cookies: true,
-      cookieOptions: {domain: '', path: ''}
-    }
-  },
-  upload: {
-    baseDir: '/Users/leob_o/Desktop/homeworks',
-    options: {
-      createParentPath: true
-    }
-  },
-  cors: {
-    options: {
-      origin: 'http://47.102.144.176',
-      credentials: true
+  development: {
+    mongodb: {
+      url: 'mongodb://localhost/homework',
+      options: {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    },
+    jwt: {
+      secret: '123456',
+      options: {
+        cookie: 'hw',
+        cookies: true,
+        cookieOptions: {domain: '', path: ''}
+      }
+    },
+    upload: {
+      baseDir: '/Users/leob_o/Desktop/homeworks',
+      options: {
+        createParentPath: true
+      }
+    },
+    cors: {
+      options: {
+        origin: 'http://127.0.0.1:8080',
+        credentials: true
+      }
     }
   }
+  
 }
+
+
+
+module.exports = process.env.NODE_ENV=='production'?credentials.production:credentials.development;
---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'homework',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      key  : '/root/.ssh/id_rsa.pub',
+      host : '47.102.144.176',
+      ref  : 'origin/master',
+      repo : 'git@github.com:LeoB-O/HomeworkbilityBackend.git',
+      path : '/data/project/HomeworkbilityBackend',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
